88b45a8be7bc4357722ccd4619187da0a5d605b1
Adam Stępień <vesemir2k@gmail.com>
2024-03-03 16:35:08 +0100

    Add main.js file and implement form validation

---
+++ main.js
@@ -0,0 +1,35 @@
+const emailInput = document.querySelector(".newsletter-form__email-input");
+const buttonSubmit = document.querySelector(".newsletter-form__button");
+const emailErrorElement = document.querySelector(
+  ".newsletter-form__email-error"
+);
+const newsletterElement = document.querySelector(".newsletter");
+const successMessageElement = document.querySelector(".success-message");
+const successMessageEmailStrong = document.querySelector(
+  ".success-message__email-strong"
+);
+
+const emailRegex =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+buttonSubmit.addEventListener("click", (event) => {
+  event.preventDefault();
+  const valueEmailInput = emailInput.value.trim();
+
+  const validateEmail = emailRegex.test(valueEmailInput);
+
+  if (valueEmailInput === "") {
+    emailErrorElement.textContent = "Valid email required";
+    emailInput.classList.add("newsletter-form__email-input--error");
+  } else if (!validateEmail) {
+    emailErrorElement.textContent = "Valid email error";
+    emailInput.classList.add("newsletter-form__email-input--error");
+  } else {
+    successMessageEmailStrong.textContent = valueEmailInput;
+    emailInput.classList.remove("newsletter-form__email-input--error");
+    newsletterElement.style.display = "none";
+    successMessageElement.style.display = "flex";
+    emailErrorElement.textContent = "";
+    emailInput.value = "";
+  }
+});
